ed5f7589627a264a5532c3bb6f08d33ba5a2c8fe
Vera <v-brus@yandex.ru>
2021-06-17 18:32:16 +0500

    fist commit

---
+++ src/Car/Car.js
@@ -0,0 +1,29 @@
+import React from 'react'
+// function Car(props) {
+//   const divStyle = {
+//     bordere: '1px solid #ccc',
+//     marginBottom: '10px',
+//     display: 'block',
+//     padding: '10px',
+//     boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+//     borderRadius: '5px',
+//   }
+//   return (
+//     <div style={divStyle}>
+//       <h2>this is car component</h2>
+//       <p>Year</p>
+//       <input />
+//       <button></button>
+//     </div>
+//   )
+// }
+
+//
+
+// const car = () => <div>this is car component</div>
+export default () => (
+  <div>
+    this is car component
+    <strong>text</strong>
+  </div>
+)
